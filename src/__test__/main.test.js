f0e5d949694c41a8b864f7592bb7661556986a05
Nicolás Cabrera <jncabdom@gmail.com>
2021-10-19 19:18:32 +0100

    Developed solution. Added tests too

--- src/__test__/main.test.js
+++ src/__test__/main.test.js
@@ -1,7 +1,34 @@
-import { dummy } from "../main";
+import { translateCode } from "../main.js";
 
-describe("Dummy", () => {
-  it("returns true", () => {
-    expect(dummy()).toEqual(true);
-  })
-})
+describe("translateCode", () => {
+  it(`Returns an error code when receiving a number that's out of range`, () => {
+    const funct = () => translateCode(10);
+
+    expect(funct).toThrow(new Error("Number out of range"));
+  });
+  it(`returns the translated code "bed" when receiving 147`, () => {
+    const translation = translateCode(147);
+
+    expect(translation).toEqual("bed");
+  });
+});
+
+describe("decodeString", () => {
+  it(`returns the translated code "bed" when receiving 147`, () => {
+    const translation = translateCode(147);
+
+    expect(translation).toEqual("bed");
+  });
+
+  it(`returns the translated code "noted" when receiving 80547`, () => {
+    const translation = translateCode(80547);
+
+    expect(translation).toEqual("noted");
+  });
+
+  it(`returns the translated code "total" when receiving 5005622`, () => {
+    const translation = translateCode(50562);
+
+    expect(translation).toEqual("total");
+  });
+});
--- src/main.js
+++ src/main.js
@@ -1,3 +1,26 @@
-export function dummy() {
-  return true;
+const MIN_NUMBER = 100;
+const MAX_NUMBER = 999999;
+
+const BASE_STRING = "oblietadnm";
+
+export const translateCode = (numberCode) => {
+  if (numOutOfRange(numberCode)) {
+    throw (new Error("Number out of range"));
+  }
+
+  return decodeString(numberCode.toString());
+}
+
+const decodeString = (numberCode) => {
+  let result = '';
+  numberCode = numberCode.toString();
+
+  for(const number of numberCode) {
+    result += BASE_STRING[number];
+  }
+  return result;
+}
+
+const numOutOfRange = (number) => {
+  return (number < MIN_NUMBER || number > MAX_NUMBER);
 }
